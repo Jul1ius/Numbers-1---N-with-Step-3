a09b9d14c68e992748bb4045150370c78b00bfa5
its julii <nanji828@gmail.com>
2022-11-29 20:20:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Numbers_1___N_with_Step_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long num = long.Parse(Console.ReadLine());
+            for (long i = 1; i <= num; i += 3)
+            {
+                Console.WriteLine(i);
+            }    
+        }
+    }
+}
